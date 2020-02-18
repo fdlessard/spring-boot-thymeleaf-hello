e6cfedd6dfcf24e79caddecc66b2adf919a6b559
Francois-David Lessard <francois-david.lessard@nbc.ca>
2020-02-18 12:29:15 -0500

    first commit

---
+++ src/main/java/io/fdlessard/codebites/thymeleaf/hello/SpringBootThymeleafHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package io.fdlessard.codebites.thymeleaf.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafHelloWorldApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootThymeleafHelloWorldApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/fdlessard/codebites/thymeleaf/hello/SpringBootThymeleafHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fdlessard.codebites.thymeleaf.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafHelloWorldApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
